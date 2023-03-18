8ec1e33f6c9bd3e3e885a7e6557f0da9d0720568
totosch <totokennedy@gmail.com>
2023-03-17 21:38:44 -0300

    changed textFieldList from an array to an ArrayList for a cleaner code

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import java.util.Random;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -41,39 +42,46 @@ public class Main {
 		frame.setBounds(100, 100, 680, 525);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new GridLayout(5, 5));
-		
-		textField = new JTextField();	
-		
+
+		textField = new JTextField();
+
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
-		
-		//8 random numbers that will become the player's objective
-		
+
+		// 8 random numbers that will become the player's objective
+
 		Random randomNumber = new Random();
 		int[] numbers = new int[8];
-		
-		for (int i = 0; i < numbers.length; i++) {
-            int num = randomNumber.nextInt(11) + 5; 
-            numbers[i] = num;
-	}
-		 //end of generating random numbers
 
-		JTextField[] arrayOfTextFields = { new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField(), new JTextField() };
-		
-		int[] textFieldsWithRandomNumbers = {3, 8, 13, 18, 19, 20, 21, 22};
-		
-		for(int i = 0; i < textFieldsWithRandomNumbers.length; i++) {			
-			int indexOfRandomPosition = textFieldsWithRandomNumbers[i];			
-			arrayOfTextFields[indexOfRandomPosition].setText(Integer.toString(numbers[i]));			
+		for (int i = 0; i < numbers.length; i++) {
+			int num = randomNumber.nextInt(11) + 5;
+			numbers[i] = num;
 		}
+
+		// end of generating random numbers
+
+
+		ArrayList<JTextField> textFieldList = new ArrayList<>();
+
+		for (int i = 0; i < 23; i++) {
+			JTextField textField = new JTextField();
+			textFieldList.add(textField);
+		}
+
+		int[] textFieldsWithRandomNumbers = { 3, 8, 13, 18, 19, 20, 21, 22 };
+
 		
+		for (int i = 0; i < textFieldsWithRandomNumbers.length; i++) {
+			int indexOfRandomPosition = textFieldsWithRandomNumbers[i];
+			textFieldList.get(indexOfRandomPosition).setText(Integer.toString(numbers[i]));
+		}
+
 		JButton checkButton = new JButton("Check!");
-		
-		for (JTextField textFields : arrayOfTextFields) {
+
+		for (JTextField textFields : textFieldList) {
 			frame.getContentPane().add(textFields);
 		}
 		frame.getContentPane().add(checkButton);
-		
 
 	}
 }
